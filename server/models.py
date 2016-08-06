a957df951edd86b5c3e8a0efc92ab0d487f14ae4
Eli Tan <eliedrian1996@gmail.com>
2016-08-06 17:49:24 +0800

    Add marital status field.

--- server/models.py
+++ server/models.py
@@ -8,16 +8,26 @@ class Person(models.Model):
     REGULAR_MEMBER = 1
     SERVICE_PROVIDER = 2
     ADMINISTRATOR = 3
-    MALE = 1
-    FEMALE = 2
     USER_TYPE_CHOICES = (
         (REGULAR_MEMBER, 'Regular Member'),
         (SERVICE_PROVIDER, 'Service Provider'),
         (ADMINISTRATOR, 'Administrator')
     )
+    MALE = 1
+    FEMALE = 2
     GENDER_CHOICES = (
         (MALE, 'Male'),
         (FEMALE, 'Female')
+    )
+    SINGLE = 1
+    MARRIED = 2
+    DIVORCED = 3
+    WIDOWED = 4
+    MARITAL_STATUS_CHOICES = (
+        (SINGLE, 'Single'),
+        (MARRIED, 'Married'),
+        (DIVORCED, 'Divorced'),
+        (WIDOWED, 'Widowed')
     )
 
     user = models.OneToOneField(User)
@@ -33,6 +43,7 @@ class Person(models.Model):
     postal_code = models.CharField(blank=False, max_length=20)
     province = models.CharField(blank=False, max_length=50)
     street = models.CharField(blank=False, max_length=100)
+    marital_status = models.SmallIntegerField(choices=MARITAL_STATUS_CHOICES, default=SINGLE)
 
     def address(self):
         address = [self.street, self.city, self.province, self.postal_code, self.country]
