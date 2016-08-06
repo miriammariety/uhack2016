fe979aed7f726b67133c68d51eae4962e7df5611
Eli Tan <eliedrian1996@gmail.com>
2016-08-06 19:55:31 +0800

    Fix urls redirecting to wrong page.

--- walang/urls.py
+++ walang/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'$', views.WalangHomeView.as_view(), name='home')
+    url(r'^$', views.WalangHomeView.as_view(), name='home')
 
 
 ]
