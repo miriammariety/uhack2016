a89812a180fd1dc02bfe896cf1b927fca530a6a9
Eli Tan <eliedrian1996@gmail.com>
2016-08-06 20:16:43 +0800

    Add services to context.

--- walang/views.py
+++ walang/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from walang.models import Person, Service
-# Create your views here.
+
+
 class WalangHomeView(TemplateView):
     template_name = "walang/home.html"
 
     def get_context_data(self, *args):
-        return super(WalangHomeView, self).get_context_data(*args)
+        context = super(WalangHomeView, self).get_context_data(*args)
+        context['services'] = Service.objects.all()
+        return context
